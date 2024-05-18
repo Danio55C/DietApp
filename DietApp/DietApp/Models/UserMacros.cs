71ddc20430cda5f9709aabdb52cff7e3b7f479de
daniel Janca <jancadaniel55@gmail.com>
2024-05-18 19:28:15 +0200

    początek pracy nad modelem makroskładnikami

---
+++ DietApp/DietApp/Models/UserMacros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace DietApp.Models
+{
+    public class UserMacros
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public int DailyCaloricLimit { get; set; }
+        public int CaloriesConsumed { get; set; }
+        public int DailyCarbsLimit { get; set; }
+        public int CarbsConsumed { get; set; }
+        public int DailyProteinLimit { get; set; }
+        public int ProteinConsumed { get; set; }
+        public int DailyFatsLimit { get; set; }
+        public int FatsConsumed { get; set; }
+
+
+    }
+}
